c5c4581cf2f414a2c43e060e774af31432b8869b
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-04-06 23:04:53 +0530

    hello

---
+++ src/Components/Info.js
@@ -0,0 +1,13 @@
+
+
+const Info = (props) => (
+    
+        <div>
+            <h1> {props.name}</h1> 
+            <p>{props.description}</p>
+        </div>
+   
+    
+)
+
+export default Info
---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+// import "../style.css"
+
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <h1>Abhishek Shankar Choudhary</h1>
+            <div className="links">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Skills</a>
+                <a href="#">Projects</a>
+                <a href="#">Blogs</a>
+               
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
